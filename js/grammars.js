547b8b4bcfbfb648272c9efd73048bb20fcbfaaf
Drew Macrae <drewmacrae@gmail.com>
2020-03-23 08:45:34 -0700

    Working on better ingredients list and adding recipies

--- js/grammars.js
+++ js/grammars.js
@@ -2,6 +2,8 @@
  * @author Drew
  */
 
+
+
 var outputFormat = "CBDQ";
 
 //Fixme I don't work with single item lists because the english grammar is hard
@@ -13,7 +15,7 @@ function listOf(listName,items){
 //ingredient to an ingredient list
 function ingredient(items){
     for (var i = items.length - 1; i >= 0; i--) {
-        items[i]="[ingredients:#ingredients#, "+items[i]+"]"+items[i]
+        items[i]="[ingredients:#ingredients#"+items[i]+"<br>]"+items[i]
     }
     //console.log(items);
     return items;
@@ -123,24 +125,25 @@ function inSeasonCookableVegetables(){
 //FIXME implementation of cookable and vegetable is clunky
 var fishList = ingredient(["salmon","fish","tuna","cod"]);
 //build lists with a mix of ingredients and non-ingredients
-var descriptiveProteinList = ingredient(["bean", "lentil", "chickpea", "chicken", "mushroom", "beef"]);
+var descriptiveProteinList = ingredient(["bean", "lentil", "chickpea", 
+	"chicken", "mushroom", "beef"]);
 descriptiveProteinList.push("#descriptiveProtein# and #vegetable#");
 descriptiveProteinList.push("#fish#");
 descriptiveProteinList.push("smoked #fish#");
 
 var proteinList = ingredient(["dried beans",
 "canned beans", "tofu", "lentils",
-    "chickpeas", "frozen chicken", "canned chicken", "foraged mushrooms", "frozen beef"]);
+    "chickpeas", "frozen chicken", "canned chicken", "foraged mushrooms", 
+    "frozen beef"]);
 proteinList.push("#protein# and #vegetable#");
-proteinList.push("#fish#");
 proteinList.push("smoked #fish#");
 proteinList.push("canned #fish#");
-proteinList.push("frozen #fish#");
+proteinList.push("#fish#");
 
 var pastaList = ingredient(["spaghetti","ziti","macaroni","tagliatelle",
     "rigatoni","linguine","penne","fettuccine","orzo"]);
-pastaList.push("[filling:#nonSauceExtra#][ingredients:#ingredients#, flour and eggs (to make pasta) or #filling# tortellini]#filling# tortellini");
-pastaList.push("[filling:#nonSauceExtraOrVegetableOrDescriptiveProtien#][ingredients:#ingredients#, flour and eggs (to make pasta) or #filling# ravioli] #filling# ravioli");
+pastaList.push("[filling:#nonSauceExtra#][ingredients:#ingredients#flour and eggs (to make pasta) or #filling# tortellini<br>]#filling# tortellini");
+pastaList.push("[filling:#nonSauceExtraOrVegetableOrDescriptiveProtien#][ingredients:#ingredients#flour and eggs (to make pasta) or #filling# ravioli<br>] #filling# ravioli");
 pastaList.push("#optAdj ##pasta#");
 
 var nonSauceExtraList = ingredient([
@@ -150,24 +153,181 @@ var nonSauceExtraList = ingredient([
             "slivered almonds",
             "capers",
             "bacon"]);
-nonSauceExtraList.push("[ingredients:#ingredients#, soba noodles, lime, sesame oil, rice wine vinegar, sugar]marinated soba noodles");
-var saucesList = ["[ingredients:#ingredients#, sour cream]#optSpice #sour cream"];
-saucesList.push("[ingredients:#ingredients#, yogurt]#optSpice #[ingredients:#ingredients#, salt and pepper to taste]yogurt sauce");
-saucesList.push("yogurt #vegetable# sauce[ingredients:#ingredients# thinly sliced, yogurt]");
-saucesList.push("[ingredients:#ingredients#, minced onion, wine, stock, butter]#optSpice #pan sauce[ingredients:#ingredients#, salt and pepper to taste]");
-saucesList.push("#optSpice ##vegetable# pesto[ingredients:#ingredients#, oil, nuts, garlic, parmesan]");
+nonSauceExtraList.push("[ingredients:#ingredients#soba noodles<br>lime<br>sesame oil<br>rice wine vinegar<br>sugar<br>]marinated soba noodles");
+var saucesList = ["[ingredients:#ingredients#sour cream<br>]#optSpice #sour cream"];
+saucesList.push("[ingredients:#ingredients#yogurt<br>]#optSpice #[ingredients:#ingredients#salt and pepper to taste<br>]yogurt sauce");
+saucesList.push("yogurt #vegetable# sauce[ingredients:#ingredients#yogurt<br>]");
+saucesList.push("[ingredients:#ingredients#minced onion<br>wine<br>#stock#<br>butter<br>]#optSpice #pan sauce[ingredients:#ingredients#salt and pepper to taste<br>]");
+saucesList.push("#optSpice ##vegetable# pesto[ingredients:#ingredients#oil<br>nuts<br>garlic<br>parmesan<br>]");
 
 if(outputFormat=="CBDQ"){
     var origin =["[recipes:]#setLets##message#"];
-    var dish = ["[ingredients:Ingredients]#dishDescription#"];
-    var side = ["[ingredients:Ingredients]#sideDescription#"];
+    var dish = ["[ingredients:][method:]#dishDescription#"];
+    var side = ["[ingredients:]#sideDescription#"];
 } else {
     var origin = ["[recipes:]#setLets##message#<br>#recipes#"];
-    var dish = ["[ingredients:Ingredients]#dishDescription#[recipes:#recipes# <br> #ingredients#]"];
-    var side = ["[ingredients:Ingredients]#sideDescription#[recipes:#recipes# <br> #ingredients#]"];
+    var dish = ["[ingredients: ]#dishDescription#[recipes:#recipes# <br> Ingredients #ingredients#]"];
+    var side = ["[ingredients: ]#sideDescription#[recipes:#recipes# <br> Ingredients #ingredients#]"];
 }
+var dishDescription = [
+			"[thisStock:#stock#]"+
+            "[ingredients:#ingredients#Oil<br>#thisStock#<br>]"+
+            "[thisProtein:#protein#]"+
+            "[method:#method#Heat oil until shimmering, before adding "+
+            "#thisProtein# and browning on high heat. Remove and set aside. "+
+            "Add vegetables and cook until they take on a little color, return "+
+            "#thisProtein# to the pan and deglazing with #thisStock#. Simmer "+
+            "on the stove or covered in a 300 degree oven for 30-60 minutes "+
+            "before seasoning to taste.]"+
+            "a stew of #listOfCookableVegetables# with #optAdj ##thisProtein#",
+            
+            "#optSpice ##listOfVegetables# soup with #protein#[ingredients:#ingredients##stock.capitalize#<br>Salt and pepper to taste<br>]",
+
+            "[ingredients:#ingredients#Rice<br>]#optAdj ##vegetable#, #protein# and rice",
+            
+            "#optAdj ##descriptiveProtein# #proteinPreparation#",
+            
+            "[ingredients:#ingredients#Beans<br>Onions<br>Tomatoes<br>Cumin<br>Chili<br>]#optAdj ##spice# chili with #protein#",
+            
+            "#pasta# with #protein# and #vegetable#",
+            
+            "#specificDish## optAddition#",
+            
+            "#vegetable# salad with #listOfVegetables# and #protein#",
+            
+            "#dishDescription# with #extra#",
+            
+            "#dishDescription## optAddition#",
+            
+            //"#dishDescription# might be nice# optAddition#",//grammatically wierd
+            "#optAdj ##descriptiveProtein# casserole with #listOfVegetables#[ingredients:#ingredients#Butter<br>Flour<br>Bread crumbs<br>]",
+        ];
+
+var stockList = ingredient(["vegetable stock", "vegetable broth", "chicken stock", "chicken broth", "mushroom broth", "white wine"]);
+
+var adjList = ["#adj#, #adj#",
+        	"[ingredients:#ingredients#Garlic<br>Butter<br>Parsely<br>]garlic",
+        	"[ingredients:#ingredients#Green-garlic<br>]green-garlic",
+        	"[ingredients:#ingredients#Garlic<br>Lemon juice<br>Lemon zest<br>]fresh","pureed","sauteed","baked","fried",
+        	"[ingredients:#ingredients#Green chile<br>]green chile",
+        	"[ingredients:#ingredients#Peppers<br>Cinnamon<br>Chocolate<br>Black pepper<br>Cumin<br>Salt to taste<br>]mole",
+        	"[ingredients:#ingredients#Szechuan peppercorn<br>Chiles<br>Garlic<br>Ginger<br>Star anise<br>Soy sauce<br>Rice wine vinegar<br>]Szechuan",
+        	"[ingredients:#ingredients#Curry<br>Water chestnuts<br>Corn<br>Carrots<br>Coconut milk<br>]Thai",
+        	"[ingredients:#ingredients#Butter<br>Flour<br>Garlic<br>Paprika<br>Cayanne pepper<br>Thyme<br>]Cajun",
+        	"[ingredients:#ingredients#Couscous<br>Za'atar<br>Pine nuts<br>]za'atar",
+        	"[ingredients:#ingredients#Lemon<br>Parsely<br>Garlic<br>Chopped tomatoes<br>]Mediterranean",
+        	"[ingredients:#ingredients##fruit.capitalize#<br>]sweet",
+        	"[ingredients:#ingredients##stock.capitalize#<br>Butter or cream<br>]rich",
+        	"[ingredients:#ingredients#Red pepper flakes<br>]spicy"];
+
+var proteinPrepList = [
+        	"[ingredients:#ingredients#Pie crust<br>Butter<br>Flour<br>Diced potatoes<br>Peas<br>Diced carrots<br>]pot-pie",
+        	"[ingredients:#ingredients#Chopped onion<br>Pepper<br>Bread crumbs<br>Bun<br>Ketchup<br>]burger"];
+
+        //specific dishes don't allow for much modification
+var specificDishList = [
+			"[ingredients:#ingredients#Oil<br>Onion<br>Tomatoes<br>Eggs<br>Salt and pepper to taste<br>]"+
+			"[method:#method#Dice the onions and brown them in a frying pan. When the onions "+
+			"start to brown, crush and add the tomatoes and cook for half an "+
+			"hour. Season with salt and pepper before forming a set of wells "+
+			"and adding an egg to each well. Let the egg cook to your "+
+			"preference and serve.<br>]"+
+			"shakshuka",
+
+            "[ingredients:#ingredients#Onions<br>Peppers<br>Oil<br>Chili powder<br>Flour tortillas<br>]"+
+            "[method:#method#Cut the vegetables into long long slices. Add "+
+            "oil to the pan and heat. Add the peppers and onions and sautee "+
+            "for twenty minutes, adding spices half way through. Serve "+
+            "immediately with warm flour tortillas.<br>]"+
+            "fajitas",
+
+            "[ingredients:#ingredients#Oil<br>Onions<br>Eggplant<br>Squash<br>Peppers<br>Salt and pepper<br>Bay leaves<br>Butter<br>]"+
+            "[method:#method#Preheat oven to 350. Oil a casserole dish before "+
+            "arranging slices of the vegetables in alternating layers, "+
+            "seasoning each layer. Top with bay leaves and slices of butter "+
+            "and bake for 60 minutes until top is browned.<br>]"+
+            "ratatouille",
+
+            "[thisProtein:#descriptiveProtein#]"+
+            "[ingredients:#ingredients#Lasagna noodles<br>Tomato sauce<br>Ricotta<br>Mozzarella<br>Parmesan<br>]"+
+            "[method:#method#If desired, parcook the noodles to make a more "+
+            "tender lasagna. Sautee #thisProtein# until browned but not cooked through. "+
+            "Preheat oven to 375 degrees, and oil a "+
+            "casserole dish. Set aside half the mozzerella and the parmesan "+
+            "for the top. Lay out a layer of sauce, then a layer of "+
+            "noodles then a layer of cheese. Repeat to use all the noodles, "+
+            "sauce and ricotta. Top with remaining cheese and bake for 35 "+
+            "minutes until the top is bubbly and lightly browned.<br>]"+
+            "#thisProtein# lasagna",
+            
+            "[thisProtein:#descriptiveProtein#]"+
+            "[ingredients:#ingredients#Onion<br>Carrots<br>Peas<br>Potatoes<br>Rasins<br>]"+
+            "[method:#method#In a dutch oven, brown #thisProtein#, onions and "+
+            "carrots. Don't crowd the pan; set aside browned ingredients "+
+            "until finished. When finished, return to pan and add other "+
+            "ingredients. Cover and cook in a 300 degree oven for 45 minutes.<br>]"+
+            "#thisProtein# tagine"];
+
+var sideDescriptionList = [
+            "[thisVegetable:#cookableVegetable#]"+
+            "[ingredients:#ingredients#Oil<br>]"+
+            "[method:Start by thinly slicing #thisVegetable#. Heat oil to 325 "+
+            "degrees and add #thisVegetable#, allowing the slices to cook "+
+            "until golden before removing them, seasoning and placing on a "+
+            "wire rack or a plate lined with paper towel.<br>#method#]"+
+            "#thisVegetable# chips",
+
+            "[ingredients:#ingredients#Oil<br>]"+
+            "[thisVegetable:cookableVegetable]"+
+            "[ingredients:#ingredients#Salt and pepper<br>]"+
+            "[Coarsely chop #thisVegetable, before cooking it in an oiled skillet and seasoning to taste as it cools.]"+
+            "sauteed #thisVegetable#",
+            
+            "#vegetable#",
+            "#spice# spiced #vegetable#",
+            "#sideDescription# and #extra#"
+        ];
 
 var grammars = {
+	recipes : {
+		"origin":"<h1>#dish.capitalize#</h1>"+
+		"<h3>Ingredients</h3>"+
+		"#ingredients#<br>"+
+		"#method#",
+		"dish":dish,
+		"dishDescription":dishDescription,
+		        "listOfVegetables": listOf("listOfVegetables","vegetable"),
+        "listOfCookableVegetables": listOf("listOfCookableVegetables","cookableVegetable"),
+
+        "vegetable": ingredient(inSeasonVegetables()),
+        "cookableVegetable": ingredient(inSeasonCookableVegetables()),
+        "fruit":["apple","orange","honey","juice"],
+        "fish":fishList,
+        "descriptiveProtein":descriptiveProteinList,
+        "protein":proteinList,
+        "pasta":pastaList,
+        "adj": adjList,
+        "stock": stockList,
+        "optAdj ":["","#adj# "],
+        "spice":ingredient(["turmeric","cinnamon","paprika","cumin","cilantro","parsely",
+        	"mint","thyme","oregano","garam masala","rosemary","sage"]),
+        "optSpice ":["a hint of #spice# on ","#spice# ",""],
+        "proteinPreparation":proteinPrepList,
+		"specificDish":specificDishList,
+
+        "!":["",".","!"],
+        "sideDescription":sideDescriptionList,
+        "nonSauceExtra":nonSauceExtraList,
+        "sauces":saucesList,
+        "extra": ["#nonSauceExtra#","#sauces#"],
+        " optAddition": [
+            ", with #sideDescription#",
+            "",
+            " with #extra#"
+        ],
+        "nonSauceExtraOrVegetableOrDescriptiveProtien":["#nonSauceExtra#","#vegetable#","#descriptiveProtein#"],
+
+	},
     dinners : {
         "origin":origin,
         "dish":dish,
@@ -180,21 +340,7 @@ var grammars = {
             "#dish.capitalize# for #savoryMeal#?",
         ],
 
-        "dishDescription":[
-            "a stew of #listOfCookableVegetables# with #optAdj ##descriptiveProtein#",
-            "#optSpice ##listOfVegetables# soup with #protein#[ingredients:#ingredients#, stock, salt and pepper to taste]",
-            "[ingredients:#ingredients#, rice]#optAdj ##vegetable#, #protein# and rice",
-            "#optAdj ##descriptiveProtein# #proteinPreparation#",
-            "[ingredients:#ingredients#, beans, onions, tomatoes, cumin, chili]#optAdj ##spice# chili with #protein#",
-            "#pasta# with #protein# and #vegetable#",
-            "#specificDish## optAddition#",
-            "#vegetable# salad with #listOfVegetables# and #protein#",
-            "#dishDescription# with #extra#",
-            "#dishDescription## optAddition#",
-            //"#dishDescription# might be nice# optAddition#",//grammatically wierd
-            "#optAdj ##descriptiveProtein# casserole with #listOfVegetables#[ingredients:#ingredients#, butter, flour, bread crumbs]",
-        ],
-
+        "dishDescription":dishDescription,
         "listOfVegetables": listOf("listOfVegetables","vegetable"),
         "listOfCookableVegetables": listOf("listOfCookableVegetables","cookableVegetable"),
 
@@ -205,43 +351,18 @@ var grammars = {
         "descriptiveProtein":descriptiveProteinList,
         "protein":proteinList,
         "pasta":pastaList,
-        "adj": ["#adj#, #adj#",
-        "[ingredients:#ingredients#, garlic, butter, parsely]garlic",
-        "[ingredients:#ingredients#, green-garlic]green-garlic",
-        "[ingredients:#ingredients#, garlic, lemon juice, lemon zest]fresh","pureed","sauteed","baked","fried",
-        "[ingredients:#ingredients#, green chile]green chile",
-        "[ingredients:#ingredients#, peppers, cinnamon, chocolate, black pepper, cumin, salt to taste]mole",
-        "[ingredients:#ingredients#, Szechuan peppercorn, chiles, garlic, ginger, star anise, soy sauce, rice wine vinegar]Szechuan",
-        "[ingredients:#ingredients#, curry, water chestnuts, corn, carrots, coconut milk]Thai",
-        "[ingredients:#ingredients#, butter, flour, garlic, paprika, cayanne pepper, thyme]Cajun",
-        "[ingredients:#ingredients#, couscous, za'atar, pine nuts]za'atar",
-        "[ingredients:#ingredients#, lemon, parsely, garlic, chopped tomatoes]Mediterranean",
-        "[ingredients:#ingredients#, #fruit#]sweet",
-        "[ingredients:#ingredients#, stock, buttor or cream]rich",
-        "[ingredients:#ingredients#, red pepper flakes]spicy"],
+        "adj": adjList,
+        "stock": stockList,
         "optAdj ":["","#adj# "],
         "spice":ingredient(["turmeric","cinnamon","paprika","cumin","cilantro","parsely",
         "mint","thyme","oregano","garam masala","rosemary","sage"]),
         "optSpice ":["a hint of #spice# on ","#spice# ",""],
-        "proteinPreparation":[
-        "[ingredients:#ingredients#, pie crust, butter, flour, diced potatoes,  peas, diced carrots]pot-pie",
-        "[ingredients:#ingredients#, chopped onion, pepper, bread crumbs, bun, ketchup]burger"],
-        //specific dishes don't allow for much modification
-        "specificDish":["[ingredients:#ingredients#, onion, tomatoes, eggs, salt and pepper to taste]shakshuka",
-            "[ingredients:#ingredients#, onions, peppers, chili powder]fajitas",
-            "[ingredients:#ingredients#, onions, eggplant, squash, peppers, bay leaves, salt and pepper to taste]ratatouille",
-            "#descriptiveProtein# [ingredients:#ingredients#, lasagna noodles, tomato sauce, ricotta, mozzarella, parmesan]lasagna",
-            "#descriptiveProtein# [ingredients:#ingredients#, onion, carrots, peas, potatoes, rasins]tagine"],
+        "proteinPreparation":proteinPrepList,
+        "specificDish":specificDishList,
         "savoryMeal":["dinner","lunch"],
         "!":["",".","!"],
         "setLets":["[lets:let's]","[lets:I'll]","[lets:you should]","[lets:we will]"],
-        "sideDescription":[
-            "#cookableVegetable# chips",
-            "[ingredients:#ingredients#, oil]sauteed #cookableVegetable#[ingredients:#ingredients#, salt and pepper to taste]",
-            "#vegetable#",
-            "#spice# spiced #vegetable#",
-            "#sideDescription# and #extra#"
-        ],
+        "sideDescription":sideDescriptionList,
         "nonSauceExtra":nonSauceExtraList,
         "sauces":saucesList,
         "extra": ["#nonSauceExtra#","#sauces#"],
